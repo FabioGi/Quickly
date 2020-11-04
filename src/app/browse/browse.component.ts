4e89b77526d72f96f9b149de30896be2a690f329
mboup <coumba.mboup@etudiant.univ-rennes1.fr>
2020-11-04 22:46:54 +0100

    liste exercices

--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -5,13 +5,21 @@ import { RouterExtensions } from '@nativescript/angular';
     selector: "Browse",
     templateUrl: "./browse.component.html"
 })
+
 export class BrowseComponent implements OnInit {
 
+    public items: string[] = [];
     constructor(public routerExtensions: RouterExtensions) {
+
         // Use the component constructor to inject providers.
     }
 
     ngOnInit(): void {
+        for (var i = 1; i < 10; i++) {
+            this.items.push("Exercice " +  i);
+        }
+
         // Use the "ngOnInit" handler to initialize data for the view.
     }
+
 }
