a0798a807ca79a164532258763642ec396e350a6
issa7 <issa.keita.etudiant.rennes1@gmail.com>
2021-01-12 22:54:50 +0100

    burgs au niveau du chargement

--- src/app/laucnh_screen_/launch_screen.component.ts
+++ src/app/laucnh_screen_/launch_screen.component.ts
@@ -13,17 +13,32 @@ export class LaunchScreenComponent implements OnInit {
 	public loadingText: String = 'loading';
 	private timeScreen = 0;
 	private count = 0;
+	private id ;
 	constructor(public routerExtensions: RouterExtensions) { }
 
 	ngOnInit() {
+
+		this.id = setInterval(()=>{
+			//burg sur le setInterval pour corriger 
+			if(this.timeScreen < 8000){ 	
 			this.timeScreen +=400;
 			if(this.count===3){
 				this.count = 0;
 				this.loadingText = 'Loading'
 			}else {
-				this.loadingText +='...';
-				this.count++;
+					this.loadingText +='.';
+					this.count++;
 			}
+			}else{
 			this.routerExtensions.navigate(['home'],{clearHistory:true});
+			}
+		},400)
+		
 	 }
+
+	 ngOnDestroy() {
+		if (this.id) {
+		  clearInterval(this.id);
+		}
+	  }
 }
